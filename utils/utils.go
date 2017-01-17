3ef071a8f9a0fd63c63460938708d887f72d500f
Scott Le <scott.lee318@gmail.com>
2017-01-18 00:58:21 +0700

    add subtract function

--- utils/utils.go
+++ utils/utils.go
@@ -13,3 +13,16 @@ func FilterStringArray(arr []string, f FilterStringArrayFunc) []string {
 
 	return result
 }
+
+func SubtractStringArray(firstArr, secondArr []string) []string {
+	secondArrHash := map[string]bool{}
+	for _, v := range secondArr {
+		secondArrHash[v] = true
+	}
+
+	filterFunc := func(value string) bool {
+		return !secondArrHash[value]
+	}
+
+	return FilterStringArray(firstArr, filterFunc)
+}
--- utils/utils_test.go
+++ utils/utils_test.go
@@ -13,3 +13,20 @@ func TestFilterStringArray(t *testing.T) {
 		t.Errorf("Result array should filter out letter a, but got %v", arr)
 	}
 }
+
+func TestSubtractStringArray(t *testing.T) {
+	firstArr := []string{"a", "b", "c", "d"}
+	secondArr := []string{"c", "d"}
+
+	result := SubtractStringArray(firstArr, secondArr)
+
+	if len(result) != 2 {
+		t.Errorf("Should get array of 2 elements. Got %v", result)
+	}
+
+	for i, v := range []string{"a", "b"} {
+		if result[i] != v {
+			t.Errorf("Expect element at %d is %s, but got %s", i, v, result[i])
+		}
+	}
+}
