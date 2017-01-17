c08fb9e0bcf77bf9275ac93fcdd1cf3080e35c85
Scott Le <scott.lee318@gmail.com>
2017-01-17 18:06:42 +0700

    refactor test

--- dbcleaner_test.go
+++ dbcleaner_test.go
@@ -1,19 +1,50 @@
 package dbcleaner_test
 
 import (
+	"database/sql"
 	"dbcleaner"
 	"testing"
 
 	_ "github.com/lib/pq"
 )
 
-func connectionString() string {
-	return "host=localhost port=5433 password=1234 username=postgres sslmode=disabled"
+const (
+	connWithDatabaseName    = "host=127.0.0.1 port=5432 password=1234 user=postgres sslmode=disable dbname=dbcleaner"
+	connWithoutDatabaseName = "host=127.0.0.1 port=5432 password=1234 user=postgres sslmode=disable"
+)
+
+func getDbConnection(conn string) *sql.DB {
+	db, err := sql.Open("postgres", conn)
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func createDatabase() {
+	db := getDbConnection(connWithoutDatabaseName)
+	defer db.Close()
+
+	_, err := db.Exec("CREATE DATABASE dbcleaner")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func dropDatabase() {
+	db := getDbConnection(connWithoutDatabaseName)
+	defer db.Close()
+
+	_, err := db.Exec("DROP DATABASE dbcleaner")
+	if err != nil {
+		panic(err)
+	}
 }
 
 func TestNewCleaner(t *testing.T) {
 	t.Run("TestRegisteredDriver", func(t *testing.T) {
-		_, err := dbcleaner.New("postgres", connectionString())
+		_, err := dbcleaner.New("postgres", connWithDatabaseName)
 
 		if err != nil {
 			t.Fatalf("Should be able to open connection to db. Err: %s", err.Error())
@@ -30,10 +61,12 @@ func TestNewCleaner(t *testing.T) {
 }
 
 func TestClose(t *testing.T) {
-	cleaner, _ := dbcleaner.New("postgres", connectionString())
-	err := cleaner.Close()
+	t.Run("Connection hasn't been closed", func(t *testing.T) {
+		cleaner, _ := dbcleaner.New("postgres", connWithDatabaseName)
+		err := cleaner.Close()
 
-	if err != nil {
-		t.Fatalf("Should be able to close connection to database. Err: %s", err.Error())
-	}
+		if err != nil {
+			t.Fatalf("Should be able to close connection to database. Err: %s", err.Error())
+		}
+	})
 }
