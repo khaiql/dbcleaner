9e6440f3084c27a86fa6aaf17be79a08a6e4afdc
Scott Le <scott.lee318@gmail.com>
2017-01-25 12:09:46 +0700

    add comment to Helper

--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,7 @@
 package helper
 
+// Helper provides a way for the cleaner to find out list of tables to work on,
+// also how to perform truncate on them
 type Helper interface {
 	GetTablesQuery() string
 	TruncateTablesCommand(tableNames []string) string
