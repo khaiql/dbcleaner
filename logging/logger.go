89b36f13bde3ea1dcc1aa9f08f38816d006f9696
Scott Le <scott.lee318@gmail.com>
2018-07-01 13:55:13 +0700

    add stdout logger

--- logging/logger.go
+++ logging/logger.go
@@ -1,5 +1,10 @@
 package logging
 
+import (
+	"fmt"
+	"os"
+)
+
 // Logger ...
 type Logger interface {
 	Println(msg string, args ...interface{})
@@ -9,3 +14,10 @@ type Logger interface {
 type Noop struct{}
 
 func (n *Noop) Println(msg string, args ...interface{}) {}
+
+// Stdout logger prints log to stdout
+type Stdout struct{}
+
+func (s *Stdout) Println(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stdout, msg+"\n", args...)
+}
